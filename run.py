7937181ee9633662bad740b7397345f297ba71ed
EdsonSMartins <smartins.edson@gmail.com>
2023-07-09 00:13:14 +0000

    Add functions to draw hangman, end game and exit program.

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from hangman_art import stages
 
-LOGO = ''' 
+LOGO = '''
  _    _                                         
 | |  | |                                        
 | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
@@ -150,28 +150,111 @@ def select_word(level):
     """
     choice = random.randrange(25)
     if level == 1:
-        hiden_word = begginer_words[choice]
+        hidden_word = begginer_words[choice]
     elif level == 2:
-        hiden_word = intermediate_words[choice]
+        hidden_word = intermediate_words[choice]
     elif level == 3:
-        hiden_word = expert_words[choice]
-    return hiden_word
+        hidden_word = expert_words[choice]
+    return hidden_word
 
 
-def show_word(hiden_word):
+def show_word(hidden_word):
     """
-   
+    display word by level   
     """
     letters = []
     to_test = []
    
-    for letter in range(len(hiden_word)):
-        letters.append(hiden_word[letter])
+    for letter in range(len(hidden_word)):
+        letters.append(hidden_word[letter])
     for letter in letters:
         to_test.append('_')
     print(stages[6])
     print(to_test)
     hangman(letters, to_test)
+
+
+def hangman(letters, to_test):
+    """
+    Tests user responses against the hidden word
+    Reveals the secret word when guessed
+
+    """ 
+    incorrect_guesses = 7
+    already_guessed = []
+
+    while '_' in to_test and incorrect_guesses > 0:
+        user_guess = input('Choose a letter: ') 
+        is_letter = check_input(user_guess)
+        print(stages[(incorrect_guesses - 1)])
+        correct_guess = 0
+        if is_letter == 2:
+            user_guess = user_guess.lower()
+            if user_guess not in already_guessed:
+                print(already_guessed)
+                already_guessed.append(user_guess)
+                # x value is used to iterate through blank list
+                x = 0
+                for letter in letters:
+                    if user_guess == letter:
+                        to_test[x] = user_guess
+                        correct_guess += 1
+                    x += 1 
+                print(to_test)
+                if correct_guess > 0:
+                    print('Well done! Correct answer!')
+                else:
+                    incorrect_guesses -= 1
+                    print('No good, wrong answer! Please try again')
+                    print(f'You have: {(incorrect_guesses)} guesses remaining')
+            else:
+                print('Letter already guessed\n Please try again!')
+        else:
+            print(f'Error: input {user_guess} is invalid')            
+    end_game(incorrect_guesses, letters)
+
+
+def end_game(incorrect_guesses, letters):
+    """
+    ends the game and displays the user's score
+    """
+    score = incorrect_guesses * len(letters)
+    print(LOGO)
+    print('Game Over!!')
+    if score > 0:
+        print(f'Congratulations!\nYour score is: {score}')
+    else:
+        print(f'The word was {letters}')
+        print("Sorry! You were unable to save the man")
+    print('Play again? \n Y. Play again N. End Game')
+    play_again = input('')
+    play_again = play_again.lower()
+    # Input validation 
+    is_valid = check_input(play_again)
+    if is_valid == 2:
+        if play_again == 'y':
+            main()
+        elif play_again == 'n':
+            exit_program()
+        else:
+            print('Please enter either Y or N')
+    else:
+        print(f'Error: input {play_again} is invalid')
+        print('Please only enter Y or N')
+
+
+def exit_program():
+    """
+    This function closes the program
+    """
+    confirm = input(
+        "Are you sure you want to exit the program? \n \
+             Press 'y' to confirm or 'n' to play again: ")
+    if confirm.lower() == "y":
+        print("Exiting program...")
+        exit()
+    else:
+        main()
 
 
 def main():
@@ -180,7 +263,9 @@ def main():
     """   
     run_game = start_game()
     if run_game == 1:
-        pass
+        difficulty = select_level()
+        hidden_word = select_word(difficulty)
+        show_word(hidden_word)
     elif run_game == 2:
         show_rules()
     elif run_game == 3:
